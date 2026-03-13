edc7ad65e223845e924e6fb5d982276daf6efe7e
Hamid FzM <hamidfzm@gmail.com>
2026-03-13 11:13:46 -0400

    feat: add glyph cask formula

---
+++ Casks/glyph.rb
@@ -0,0 +1,22 @@
+cask "glyph" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/hamidfzm/glyph/releases/download/v#{version}/Glyph_#{version}_universal.dmg"
+  name "Glyph"
+  desc "Cross-platform markdown viewer"
+  homepage "https://github.com/hamidfzm/glyph"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Glyph.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.hamidfzm.glyph",
+    "~/Library/Caches/com.hamidfzm.glyph",
+    "~/Library/Preferences/com.hamidfzm.glyph.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-tap
+
+Homebrew tap for [Glyph](https://github.com/hamidfzm/glyph) and other tools.
+
+## Usage
+
+```bash
+brew tap hamidfzm/tap
+brew install --cask glyph
+```
