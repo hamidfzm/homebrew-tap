fd6717ef01c630de47e156ee95d1ded5e0aa8a99
Hamid Feizabadi <hamidfzm@gmail.com>
2026-06-28 13:57:15 +0000

    chore: update glyph to 0.14.0

--- Casks/glyph.rb
+++ Casks/glyph.rb
@@ -1,11 +1,11 @@
 cask "glyph" do
-  version "0.12.0"
-  sha256 "c86e887842513f90737ea83154ef30b5963ae65dd0d005ac6009fccf3dc2539f"
+  version "0.14.0"
+  sha256 "14be5fc5b516515c9136528504265bf56297c9ea141ae7e929a57f5344989b80"
 
   url "https://github.com/hamidfzm/glyph/releases/download/v#{version}/Glyph_#{version}_universal.dmg"
   name "Glyph"
   desc "Cross-platform markdown viewer"
-  homepage "https://github.com/hamidfzm/glyph"
+  homepage "https://glyph-md.github.io"
 
   livecheck do
     url :url
